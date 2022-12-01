a3c06011c78dc2edd1e4439161e5c66d963728e4
Aakash Sharma <41850221+AakashUchiha@users.noreply.github.com>
2022-12-01 08:50:17 +0530

    Add files via upload

---
+++ call letters.R
@@ -0,0 +1,82 @@
+library('RSelenium')
+library(tidyverse)
+library(tidyr)
+library(netstat)   #used for port
+library(mailR)
+library("gmailr")
+require(mailR)
+
+
+
+##################    USER INPUTS     ##############################
+
+Qtr<-2 #(quarter of the desired report)
+
+#####################################################
+
+
+
+#functions used
+`%notin%`<- negate(`%in%`)
+
+#kill java process
+system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE)
+
+#starting the serveer
+rs_driver_object<-rsDriver(
+  browser="chrome",
+  chromever = '107.0.5304.62',
+  port= free_port()
+)
+
+remDr<-rs_driver_object$client
+
+remDr$open()
+
+#call report data
+
+remDr$navigate("https://ncua.gov/analysis/credit-union-corporate-call-report-data/quarterly-data")
+
+
+repeat {
+  
+  remDr$refresh()
+  Sys.sleep(100)
+  login_xpath<-paste("/html/body/div/div[1]/main/div/div/div[3]/article/div/div[1]/table/tbody/tr[1]/td[",Qtr+1,"]/a",sep="")
+  login_link <- remDr$findElement(using = "xpath", value = login_xpath)
+  t<-try(login_link)
+
+  if ("try-error" %notin% class(t)){
+    break
+  }
+}
+
+
+login_link$clickElement()
+
+
+
+
+#sending mail
+
+send.mail(from = "sehgal.rohan007@gmail.com",
+          to = "sehgal.rohan007@gmail.com",
+          subject = "Quarterly call report file available",
+          body = "Body of the email",
+          smtp = list(host.name = "smtp.gmail.com", port = 465, user.name = "sehgal.rohan007@gmail.com", passwd = "mmlclujyhdfvqqbr", ssl = TRUE),
+          authenticate = TRUE,
+          send = TRUE)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
